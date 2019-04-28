368e6c096e948f3f49bbc59267a6363ea6b7e5c8
Juan Sebastian Palma <38446282+Millionhat@users.noreply.github.com>
2019-04-28 12:43:15 -0500

    Revert "implementado produccion equivalente"
    This reverts commit 4b7a9df53c116b61bb95c8c851e1bf81a409e56d.

--- CosteoProcesos/src/modelo/Costeo.java
+++ CosteoProcesos/src/modelo/Costeo.java
@@ -1,9 +1,7 @@
 package modelo;
 
 public class Costeo {
-	public static final int PEPS=0;
-	public static final int PONDERADO=0;
-	
+
 	private double inProcess;
 	private double MODProcess;
 	private double CIFProcess;
@@ -16,9 +14,6 @@ public class Costeo {
 	private double MODStarted;
 	private double CIFstarted;
 	private double MDStarted;
-	private double percentMODStarted;
-	private double percentMDStarted;
-	private double percentCIFStarted;
 	
 	private double finished;
 	private double MODSFinished;
@@ -26,46 +21,32 @@ public class Costeo {
 	private double MDFinished;
 	private double percentMODFinished;
 	private double percentMDFinished;
-	private double percentCIFFinished;
+	private double percentCIFinished;
 	
-	private int pepsOPonderado;
+	private boolean PepsOPonderado;
 	
-	private double[][] produccionEquivalente;
 	public Costeo() {
 		
 	}
 	public double flujoFisico() {
-		return inProcess+started;
+		return IIP+UE;
 	}
 	
-	public void produccionEquivalente() {
-		produccionEquivalente=new double [4][3];
-		if(pepsOPonderado==PEPS) {
-		produccionEquivalente[0][0]=(1-percentMDProcess)*inProcess;
-		produccionEquivalente[0][1]=(1-percentMODProcess)*inProcess;
-		produccionEquivalente[0][2]=(1-percentCIFProcess)*inProcess;
-		}else if(pepsOPonderado==PONDERADO) {
-			for(int i=0;i<3;i++) {
-				produccionEquivalente[0][i]=inProcess;
-			}
+	public double[][] produccionEquivalente() {
+		double produccionEquivalente[][]=new double [4][3];
+		for(int i=0;i<3;i++) {
+			produccionEquivalente[0][i]=IIP;
 		}
-		produccionEquivalente[1][0]=started;
-		produccionEquivalente[1][1]=started;
-		produccionEquivalente[1][2]=started;
-		produccionEquivalente[2][0]=percentMDFinished*finished;		
-		produccionEquivalente[2][1]=percentMODFinished*finished;
-        produccionEquivalente[2][2]=percentCIFFinished*finished;
-        for(int i=0;i<3;i++) {
-        	for (int j=0; j<3;j++){
-               produccionEquivalente[3][i]+=produccionEquivalente[j][i];
-        	}
-        }
-	}
-	
-	public double[][] darPE(){
+		produccionEquivalente[1][0]=
+		produccionEquivalente[1][1]=
+		produccionEquivalente[1][2]=
+		produccionEquivalente[2][0]=
+		produccionEquivalente[2][1]=
+        produccionEquivalente[2][2]=
+        produccionEquivalente[3][0]=
+        produccionEquivalente[3][1]=
+        produccionEquivalente[3][2]=
+
 		return produccionEquivalente;
 	}
-	
-	public 
-	
 }
