b4818cb6a25dabe9574a4185f5e3cf95cf921ee9
Juan Sebastian Palma <38446282+Millionhat@users.noreply.github.com>
2019-04-28 10:55:47 -0500

    agregados variables necesarias

--- CosteoProcesos/src/modelo/Costeo.java
+++ CosteoProcesos/src/modelo/Costeo.java
@@ -2,9 +2,31 @@ package modelo;
 
 public class Costeo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private double inProcess;
+	private double MODProcess;
+	private double CIFProcess;
+	private double MDProcess;
+	private double percentMODProcess;
+	private double percentMDProcess;
+	private double percentCIFProcess;
+	
+	private double started;
+	private double MODStarted;
+	private double CIFstarted;
+	private double MDStarted;
+	
+	private double finished;
+	private double MODSFinished;
+	private double CIFFinished;
+	private double MDFinished;
+	private double percentMODFinished;
+	private double percentMDFinished;
+	private double percentCIFinished;
+	
+	private boolean PepsOPonderado;
+	
+	public Costeo() {
+		
 	}
-
+	
 }
--- CosteoProcesos/src/modelo/Ponderado.java
+++ CosteoProcesos/src/modelo/Ponderado.java
@@ -1,5 +1,42 @@
 package modelo;
 
 public class Ponderado {
-
+private double IIP;
+	private double UE;
+	private double UT;
+	
+	private double percentMDIIP;
+	private double percentMODIIP;
+	private double percentCIFIIP;
+	
+	private double MODUE;
+	private double MDUE;
+	private double CIFUE;
+	
+	private double percentMDUT;
+	private double percentMODUT;
+	private double percentCIFUT;
+	
+	public Ponderado() {
+		
+	}
+	
+	public double flujoFisico() {
+		return IIP+UE;
+	}
+	
+	public double[][] produccionEquivalente() {
+		double produccionEquivalente[][]=new double [4][4];
+		for(int i=0;i<4;i++) {
+			produccionEquivalente[0][i]=IIP;
+		}
+		produccionEquivalente[1][]=
+		produccionEquivalente[1][]=
+		produccionEquivalente[1][]=
+		produccionEquivalente[1][]=
+		produccionEquivalente[1][]=
+        produccionEquivalente[1][]=
+        		
+		return produccionEquivalente;
+	}
 }
