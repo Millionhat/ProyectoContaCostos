30e84e2d552cba45896396be36ba96f2167887a6
Marco Perez <marcopza99@hotmail.com>
2019-04-28 12:26:58 -0500

    Estructura básica de la ventana principal

--- CosteoProcesos/src/controller/MainWindowController.java
+++ CosteoProcesos/src/controller/MainWindowController.java
@@ -1,5 +1,71 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class MainWindowController {
 
+    @FXML
+    private TextField unitsInitialTF;
+
+    @FXML
+    private TextField initPercentMDTF;
+
+    @FXML
+    private TextField initValueMDTF;
+
+    @FXML
+    private TextField initPercentMODTF;
+
+    @FXML
+    private TextField initValueMODTF;
+
+    @FXML
+    private TextField initPercentCIFTF;
+
+    @FXML
+    private TextField initValueCIFTF;
+
+    @FXML
+    private TextField finUnitsTF;
+
+    @FXML
+    private TextField finPercentMDTF;
+
+    @FXML
+    private TextField finPercentMODTF;
+
+    @FXML
+    private TextField finPercentCIFTF;
+
+    @FXML
+    private TextField unitsStartedEndedTF;
+
+    @FXML
+    private TextField valueMDTF;
+
+    @FXML
+    private TextField valueMODTF;
+
+    @FXML
+    private TextField valueCIFTF;
+
+    @FXML
+    private Button pepsButton;
+
+    @FXML
+    private Button ponderadoButton;
+
+    @FXML
+    void pepsPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ponderadoPressed(ActionEvent event) {
+
+    }
+    
 }
