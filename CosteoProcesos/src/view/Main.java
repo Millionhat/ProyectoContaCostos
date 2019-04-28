0f4993e6234b722257098eedb9d4e1a720a35ccc
Marco Perez <marcopza99@hotmail.com>
2019-04-28 11:31:33 -0500

    Conecta con el FXML

--- CosteoProcesos/src/view/Main.java
+++ CosteoProcesos/src/view/Main.java
@@ -1,10 +1,35 @@
 package view;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		launch();
+		
 	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/view/MainWindow.fxml"));
+		Parent root = loader.load();
+		Scene mainScene = new Scene(root);
+		primaryStage.setScene(mainScene);
+		primaryStage.setTitle("Costeo por Procesos");
+		primaryStage.show();
+		
+	}
+	
+	@Override
+	public void stop() {
+		System.exit(0);
+	}
+	
 
 }
