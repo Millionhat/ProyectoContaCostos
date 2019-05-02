6a35810cd2e1244ba6a9cb0ab6d01eebef1b62f9
Juan Sebastian Palma <38446282+Millionhat@users.noreply.github.com>
2019-05-02 14:08:26 -0500

    Revert "Aplicación terminado"
    This reverts commit b20828fbbf0abfefef517fbb6cea6dbde23a18d5.

--- CosteoProcesos/src/controller/MainWindowController.java
+++ CosteoProcesos/src/controller/MainWindowController.java
@@ -72,6 +72,7 @@ public class MainWindowController implements Initializable {
     @FXML
     private Button ponderadoButton;
     private Costeo costeo;
+    
 
     @FXML
     void pepsPressed(ActionEvent event) {
@@ -117,42 +118,7 @@ public class MainWindowController implements Initializable {
 
     @FXML
     void ponderadoPressed(ActionEvent event) {
-    	double[] info = new double[15];
-    	try {
-			info[0] = Double.parseDouble(unitsInitialTF.getText());
-			info[1] = Double.parseDouble(initPercentMDTF.getText());
-			info[2] = Double.parseDouble(initValueMDTF.getText());
-			info[3] = Double.parseDouble(initPercentMODTF.getText());
-			info[4] = Double.parseDouble(initValueMODTF.getText());
-			info[5] = Double.parseDouble(initPercentCIFTF.getText());
-			info[6] = Double.parseDouble(initValueCIFTF.getText());
-			info[7] = Double.parseDouble(unitsStartedEndedTF.getText());
-			info[8] = Double.parseDouble(valueMDTF.getText());
-			info[9] = Double.parseDouble(valueMODTF.getText());
-			info[10] = Double.parseDouble(valueCIFTF.getText());
-			info[11] = Double.parseDouble(finUnitsTF.getText());
-			info[12] = Double.parseDouble(finPercentMDTF.getText());
-			info[13] = Double.parseDouble(finPercentMODTF.getText());
-			info[14] = Double.parseDouble(finPercentCIFTF.getText());
-			costeo = new Costeo(info, 1);
-			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(Main.class.getResource("/view/PonderadoView.fxml"));
-			Parent root = (Parent) loader.load();
-			PonderadoViewController controller = (PonderadoViewController)loader.getController();
-			controller.calculate(costeo);
-			Scene scene = new Scene(root);
-			Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-			stage.setOnCloseRequest(e -> controller.shutdown());
-			stage.setScene(scene);
-			stage.show();
-    	}catch(Exception e) {
-    		Alert alert = new Alert(AlertType.WARNING);
-			alert.setTitle("Cuidado!");
-			alert.setHeaderText(null);
-			alert.setContentText("Por favor complete todos los datos.");
-			alert.showAndWait();
-			e.printStackTrace();
-    	}
+
     }
 
 	@Override
